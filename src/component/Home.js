7bdffbe9975e98db23b2a636eb2395a86d435a06
Dreymi <Dr3ymi@gmail.com>
2022-04-26 11:57:56 +0100

    done

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,24 +1,22 @@
-import { render } from "@testing-library/react";
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 
-
 function Home() {
   return (
     <div className="homepage">
-      <h1 className='tastyText' >Tasty </h1>
+      <h1 className="tastyText">Tasty </h1>
       <p className="tastyBody">THE BEST RESTAURANT IN TOWN</p>
       <div className="buttons">
-      <Link to="/menu">
-      <button className="menuButton">OUR MENU</button>
-      </Link>
-      <Link to="/contact">
-      <button className="contactButton">CONTACT US</button>
-      </Link>
+        <Link to="/menu">
+          <button className="menuButton">OUR MENU</button>
+        </Link>
+        <Link to="/contact">
+          <button className="contactButton">CONTACT US</button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
