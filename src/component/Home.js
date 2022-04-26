ea97c420139774491b0e6bf33639a9d4e2324e78
Dreymi <Dr3ymi@gmail.com>
2022-04-26 11:26:15 +0100

    Merge branch 'patch-1' of https://github.com/Marta233/SideHustle-Capstone-Project-1 into Marta233-patch-1

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,7 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 
 function Home() {
@@ -10,8 +11,12 @@ function Home() {
       <h1 className='tastyText' >Tasty </h1>
       <p className="tastyBody">THE BEST RESTAURANT IN TOWN</p>
       <div className="buttons">
+      <Link to="/menu">
       <button className="menuButton">OUR MENU</button>
+      </Link>
+      <Link to="/contact">
       <button className="contactButton">CONTACT US</button>
+      </Link>
       </div>
     </div>
   )
